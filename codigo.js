dea723d09e3fd6c600564b143ed4d04cf142a6ce
ThomasMontana <tom_tain@hotmail.com>
2021-10-08 00:58:08 -0300

    hola

--- codigo.js
+++ codigo.js
@@ -1,6 +1,13 @@
-let piso = "";
+let info = "";
 
 const tipear = (Numero) => {
-    piso = piso + Numero;
-    document.getElementById("piso").value = piso;
+    info = info + Numero;
+
+    if (info.length <= 2) {
+        document.getElementById("piso").value = info
+    } else if (info.length <= 3) {
+        document.getElementById("depto").value = info
+    } else {
+        info = alert("ERROR");
+    }
 }
