ddc5f109f4a52c19bf7cc660db9ef9a22c9504d4
ThomasMontana <tom_tain@hotmail.com>
2021-10-12 17:51:13 -0300

    act

--- codigo.js
+++ codigo.js
@@ -4,10 +4,21 @@ const tipear = (Numero) => {
     info = info + Numero;
 
     if (info.length <= 2) {
-        document.getElementById("piso").value = info
-    } else if (info.length <= 3) {
-        document.getElementById("depto").value = info
+        document.getElementById("piso").value = info;
     } else {
-        info = alert("ERROR");
+        document.getElementById("depto").value = Numero;
     }
+}
+
+const Llamar = () => {
+    mensaje = ("hola mundo");
+    document.getElementById("llamar").value = (`LLamando al piso ${info}, departamento ${Numero}`);
+}
+
+const Borrar = () => {
+    info = "";
+    Numero = "";
+    document.getElementById("piso").value = info;
+    document.getElementById("depto").value = Numero;
+
 }
